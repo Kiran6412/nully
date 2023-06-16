15e1ce6b1a5c538c642208304ed0a8b666345359
Kiran6412 <136492278+Kiran6412@users.noreply.github.com>
2023-06-16 09:13:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "25562244"))
-API_HASH = os.environ.get("API_HASH", "afa60b3fd1ba44e46851f534177a9161")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6225190414:AAEOCnW2cBFgGjb0tul7h4BM5AUI02BM3Jw")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5790359537")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "22313424"))
+API_HASH = os.environ.get("API_HASH", "2cbd9a90ac99583580d9ad6150701045")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5802403990:AAEXvxnGO-_sCg81zYhm67x63d2ghKTby2w")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("1700238703")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Dhggg")
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://ciros42580:omprtngslb@cluster0.phrmyvu.mongodb.net/?retryWrites=true&w=majority") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "5790359537")) 
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://kiranu:kirankabot@cluster0.k5tx2bb.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "1700238703")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(5790359537)
+ADMINS.append(1700238703)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001795283531")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "popcornpanda") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001946243122")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "tulinks_official") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
